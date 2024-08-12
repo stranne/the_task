b335c2e464334a99e82e701971e1eeb9389ceff6
Olof Stranne <olof.stranne@gmail.com>
2024-08-12 22:50:24 +0200

    Return to list after removing a task

--- lib/ui/views/task_item/task_item_viewmodel.dart
+++ lib/ui/views/task_item/task_item_viewmodel.dart
@@ -1,9 +1,12 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 import 'package:the_task/app/app.locator.dart';
 import 'package:the_task/models/task.dart';
 import 'package:the_task/services/task_service.dart';
 
 class TaskItemViewModel extends BaseViewModel {
-  Future<void> deleteAsync(Task task) async =>
-      await locator<TaskService>().deleteAsync(task);
+  Future<void> deleteAsync(Task task) async {
+    await locator<TaskService>().deleteAsync(task);
+    locator<NavigationService>().back();
+  }
 }
